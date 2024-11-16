978fed14cf802f143a3bf2a1e7655db899b7f574
PanMinThai <146918562+PanMinThai@users.noreply.github.com>
2024-11-17 03:35:59 +0700

    Initial commit: Add Angular project

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
 
-export const routes: Routes = [];
+export const appRoutes: Routes = [
+    { path: '', component: LoginComponent },  // Đặt LoginComponent là route mặc định
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(appRoutes)],  // Sửa từ 'routes' thành 'appRoutes'
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
